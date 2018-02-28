2932a2185a02c8ee716119c38845be6e1a514e79
Matheus Rezende <matheus.rezende10@gmail.com>
2018-02-28 23:55:09 +0700

    added redux-thunk support

---
+++ thunks/location.js
@@ -0,0 +1,27 @@
+import {
+  REDUX_SAGA_LOCATION_ACTION_REQUEST,
+  REDUX_SAGA_LOCATION_ACTION_SET_POSITION,
+  REDUX_SAGA_LOCATION_ACTION_SET_ERROR,
+} from '../actions';
+
+export const getCurrentPositionThunk = (options) => dispatch => {
+    dispatch({type: REDUX_SAGA_LOCATION_ACTION_REQUEST})
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        dispatch({type: REDUX_SAGA_LOCATION_ACTION_SET_POSITION, position})
+      },
+      (error) => dispatch({type: REDUX_SAGA_LOCATION_ACTION_SET_ERROR, error}),
+      options
+    );
+}
+
+export const watchCurrentPositionThunk = (options) => (dispatch) =>{
+    dispatch({type: REDUX_SAGA_LOCATION_ACTION_REQUEST})
+    navigator.geolocation.watchPosition(
+      position => {
+        dispatch({type: REDUX_SAGA_LOCATION_ACTION_SET_POSITION, position})
+      },
+      (error) => dispatch({type: REDUX_SAGA_LOCATION_ACTION_SET_ERROR, error}),
+      options
+    );
+}
